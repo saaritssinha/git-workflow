76945af48d251a36a353d2b7a2412558f967f982
Ubuntu <ubuntu@ip-172-31-2-235.us-east-2.compute.internal>
2022-01-05 13:56:20 +0000

    main file 1st in master

---
+++ main.c
@@ -0,0 +1,21 @@
+pseudo code
+{
+Initial Function()
+{
+  //Sample Code
+}
+
+Initial Security()
+{
+<<<<<<< HEAD
+feature1 security patch()
+=======
+feature2 security patch()
+>>>>>>> feature2
+       
+	//Sample Code
+}
+
+
+
+}
